672102d1325c97f2f3b5fb9a4d1ea999a4b18e17
Manu Cornet <m@ma.nu>
2020-07-10 17:57:08 +0000

    More renaming into views

---
+++ js/rankview.js
@@ -0,0 +1,180 @@
+/** @constructor @implements {View} */
+let RankView = function(dataProvider, nav) {
+  /** @private @const {DataProvider} */
+  this.dataProvider_ = dataProvider;
+
+  /** @const @private {Nav} */
+  this.nav_ = new Nav();
+};
+
+RankView.CONTINENT_COLORS = {
+  'O': '#b600ff',  // purple
+  'S': '#0c1fb4',  // dark blue
+  'N': '#0060ff',  // blue
+  'E': '#00b31a',  // green
+  'A': '#bb9900',  // yellow
+  'P': '#e37300',  // orange
+  'Z': '#e90000',  // red
+};
+
+let maxGraphedValue = 0;
+let maxWidth = 0;
+let showDeathCounts = false;
+
+let rank;
+function rankInit() {
+  rank = new RankView(new DataProvider(
+      'https://raw.githubusercontent.com/ghdsi/covid-19/master/'), new Nav());
+  rank.init();
+}
+
+RankView.prototype.init = function() {
+  const dp = this.dataProvider_;
+  let self = this;
+  dp.fetchCountryNames().
+      then(dp.fetchJhuData.bind(dp)).
+      then(self.showRankPage.bind(self));
+  this.nav_.setupTopBar();
+};
+
+RankView.prototype.onToggleClicked = function(e) {
+  let toggle = document.getElementById('toggle');
+  if (toggle.firstChild == e.target) {
+    toggle.firstChild.classList.add('active');
+    toggle.lastChild.classList.remove('active');
+    showDeathCounts = false;
+  } else if (toggle.lastChild == e.target) {
+    toggle.firstChild.classList.remove('active');
+    toggle.lastChild.classList.add('active');
+    showDeathCounts = true;
+  }
+  this.onModeToggled();
+}
+
+RankView.prototype.onModeToggled = function() {
+  const aggregates = this.dataProvider_.getAggregateData();
+  const key = showDeathCounts ? 'deaths' : 'cum_conf';
+  let maxValue = 0;
+  let dates = Object.keys(aggregates).sort();
+
+  for (let date in aggregates) {
+    for (let country in aggregates[date]) {
+      maxValue = Math.max(maxValue, aggregates[date][country][key]);
+    }
+  }
+  maxGraphedValue = Math.log10(maxValue);
+  this.showRankPageAtCurrentDate();
+}
+
+RankView.prototype.showRankPage = function() {
+  let container = document.getElementById('data');
+  container.innerHTML = '';
+  maxWidth = Math.floor(container.clientWidth);
+
+  let i = 0;
+  let countries = this.dataProvider_.getCountries();
+  for (let code in countries) {
+    const c = countries[code];
+    let el = document.createElement('div');
+    el.setAttribute('id', code);
+    el.classList.add('bar');
+    const color = RankView.CONTINENT_COLORS[c.getContinent()];
+    el.style.backgroundColor = color;
+    el.style.color = '#fff';
+    let startSpan = document.createElement('span');
+    startSpan.classList.add('start');
+    let endSpan = document.createElement('span');
+    endSpan.classList.add('end');
+    startSpan.textContent = c.getName();
+    startSpan.style.backgroundColor = color;
+    el.appendChild(endSpan);
+    el.appendChild(startSpan);
+    container.appendChild(el);
+    i++;
+  }
+
+  this.onModeToggled();
+  let self = this;
+  container.onwheel = function(e) {
+    self.onRankWheel(e)
+  };
+  container.ontouchmove = function(e) {
+    e.preventDefault();
+    self.onRankTouchMove(e['touches'][0].clientY - currentTouchY)
+  };
+  container.ontouchstart = function(e) {
+    e.preventDefault();
+    currentTouchY = e['touches'][0].clientY;
+  }
+  container.ontouchend = function(e) {
+    e.preventDefault();
+    currentTouchY = -1;
+  }
+
+  let toggle = document.getElementById('toggle');
+  // Assume only two modes here.
+  toggle.firstChild.onclick = this.onToggleClicked.bind(this);
+  toggle.lastChild.onclick = this.onToggleClicked.bind(this);
+}
+
+RankView.prototype.onRankTouchMove = function(delta) {
+  const points_per_step = 150;
+  this.rankAdvance(delta > 0, Math.floor(Math.abs(delta / points_per_step)));
+}
+
+RankView.prototype.onRankWheel = function(e) {
+  e.preventDefault();
+  this.rankAdvance(e.deltaY > 0, 1);
+}
+
+RankView.prototype.rankAdvance = function(forward, steps) {
+  let newDateIndex = currentDateIndex + (forward ? steps : -steps);
+  newDateIndex = Math.max(newDateIndex, 0);
+  newDateIndex = Math.min(newDateIndex,
+                          this.dataProvider_.getDates().length -1);
+  currentDateIndex = newDateIndex;
+  this.showRankPageAtCurrentDate();
+}
+
+RankView.prototype.showRankPageAtCurrentDate = function() {
+  const date = this.dataProvider_.getDates()[currentDateIndex];
+  document.getElementById('title').textContent = date;
+  const data = this.dataProvider_.getAggregateData()[date];
+  const y_step = 33;
+  let container = document.getElementById('data');
+  const key = showDeathCounts ? 'deaths' : 'cum_conf';
+  let o = {};
+  for (let i = 0; i < data.length; i++) {
+    const item = data[i];
+    o[item['code']] = item[key];
+  }
+  let bars = [...document.getElementsByClassName('bar')];
+  bars = bars.sort(function(a, b) {
+    const a_code = a.getAttribute('id');
+    const b_code = b.getAttribute('id');
+    const a_count = o[a_code] || 0;
+    const b_count = o[b_code] || 0;
+    return a_count < b_count ? 1 : -1;
+  });
+  let y = 0;
+  for (let i = 0; i < bars.length; i++) {
+    let b = bars[i];
+    const code = b.getAttribute('id');
+    if (!o[code]) {
+      b.style.display = 'none';
+      continue;
+    }
+    const case_count = o[code];
+    b.getElementsByClassName('end')[0].textContent = case_count.toLocaleString();
+    b.style.display = 'block';
+    b.style.top = y + 'px';
+    b.style.width = Math.floor(
+        maxWidth * Math.log10(case_count) / maxGraphedValue);
+    y += 37;
+  }
+  for (let i = 0; i < data.length; i++) {
+    const item = data[i];
+  }
+}
+
+globalThis['rankInit'] = rankInit;
---
+++ js/syncview.js
@@ -0,0 +1,102 @@
+/** @constructor */
+let SyncView = function(dataProvider, nav) {
+  /** @private @const {DataProvider} */
+  this.dataProvider_ = dataProvider;
+
+  /** @const @private {Nav} */
+  this.nav_ = new Nav();
+};
+
+const STARTING_CASE_COUNT = 10000;
+
+SyncView.prototype.init = function() {
+  let self = this;
+  const dp = this.dataProvider_;
+  dp.fetchCountryNames().
+      then(dp.fetchJhuData.bind(dp)).
+      then(self.showSyncPage.bind(self));
+  this.nav_.setupTopBar();
+};
+
+SyncView.prototype.showSyncPage = function() {
+  const aggregates = this.dataProvider_.getAggregateData();
+  let dates = Object.keys(aggregates);
+  // Sort in chronological order.
+  dates = dates.sort();
+  let o = {'dates': dates};
+  let curveStart = {};
+  let curves = {};
+  for (let i = 0; i < dates.length; i++) {
+    const date = dates[i];
+    let items = aggregates[date];
+    for (let j = 0; j < items.length; j++) {
+      const code = items[j]['code'];
+      const cases = items[j]['cum_conf']
+      if (!curveStart[code] && cases >= STARTING_CASE_COUNT) {
+        curveStart[code] = date;
+      }
+      if (!!curveStart[code]) {
+        if (!curves[code]) {
+          curves[code] = [];
+        }
+        curves[code].push([date, cases]);
+      }
+    }
+  }
+
+  let maxDays = 0;
+  for (let code in curves) {
+    maxDays = Math.max(maxDays, curves[code].length);
+  }
+
+  let labels = [];
+  let dataToPlot = [];
+  for (let i = 0; i < maxDays; i++) {
+    labels.push('D + ' + i);
+  }
+  let i = 0;
+  for (let code in curves) {
+    const country = this.dataProvider_.getCountry(code);
+    const name = country.getName();
+    let thisData = [];
+      for (let j = 0; j < curves[code].length; j++) {
+        const caseCount = curves[code][j][1];
+        const percentage = (caseCount / country.getPopulation() * 1000).toFixed(2);
+        thisData.push(percentage);
+    }
+    const color = Graphing.CURVE_COLORS[i % Graphing.CURVE_COLORS.length];
+    dataToPlot.push({'data': thisData, 'label': name, 'borderColor': color});
+    i++;
+  }
+
+  let container = document.getElementById('data');
+  container.innerHTML = '';
+  let canvas = document.createElement('canvas');
+  canvas.setAttribute('width', container.clientWidth + 'px');
+  canvas.setAttribute('height', container.clientHeight + 'px');
+  container.appendChild(canvas);
+  let ctx = canvas.getContext('2d');
+  let cfg = Graphing.CHART_CONFIG;
+  cfg['options']['tooltips']['mode'] = 'nearest';
+  cfg['options']['tooltips']['callbacks'] = {'label': function(item, data) {
+    return data['datasets'][item['datasetIndex']]['label'] + ': ' + item['yLabel'] + ' ‰';
+  }};
+  cfg['options']['scales']['xAxes'][0]['type'] = undefined;
+  cfg['options']['scales']['xAxes'][0]['time'] = {};
+
+  cfg['data'] = {
+    'labels': labels,
+    'datasets': dataToPlot,
+  }
+
+  new Chart(ctx, cfg);
+}
+
+let sync;
+function syncInit() {
+  sync = new SyncView(new DataProvider(
+      'https://raw.githubusercontent.com/ghdsi/covid-19/master/'), new Nav());
+  sync.init();
+}
+
+globalThis['syncInit'] = syncInit;
