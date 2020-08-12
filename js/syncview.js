d6e16c6688c2f31c9ac6bb295e4a4eca322dcca7
Manu Cornet <m@ma.nu>
2020-08-12 12:17:36 +0000

    Make filters work in 'sync'

--- js/syncview.js
+++ js/syncview.js
@@ -2,6 +2,12 @@ class SyncView extends View {
 
 constructor(dataProvider) {
   super(dataProvider);
+
+  /** @private {Array.<string>} */
+  this.countriesSortedByAffectedness_ = [];
+
+  /** @private {Array.<string>} */
+  this.countriesSortedByPopulation_ = [];
 }
 
 getId() {
@@ -13,7 +19,12 @@ getTitle() {
 };
 
 renderGraph(container, labels, dataToPlot) {
-  let canvas = document.createElement('canvas');
+  let canvas = document.getElementById('sync-graph');
+  if (!canvas) {
+    canvas = document.createElement('canvas');
+    canvas.setAttribute('id', 'sync-graph');
+  }
+  canvas.innerHTML = '';
   canvas.setAttribute('width', container.clientWidth + 'px');
   canvas.setAttribute('height', Math.floor(0.8 * container.clientHeight) + 'px');
   container.appendChild(canvas);
@@ -38,6 +49,10 @@ renderGraph(container, labels, dataToPlot) {
 }
 
 prepareGraphData() {
+  if (!this.countriesSortedByPopulation_.length) {
+    this.cacheCountriesSortedByPopulation();
+  }
+
   const aggregates = this.dataProvider_.getAggregateData();
   let dates = Object.keys(aggregates);
   // Sort in chronological order.
@@ -75,6 +90,7 @@ prepareGraphData() {
   }
   let i = 0;
   let maxValue = 0;
+  let latestPercentageByCountry = {};
   for (let code in curves) {
     const country = this.dataProvider_.getCountry(code);
     if (!country) {
@@ -89,28 +105,91 @@ prepareGraphData() {
       for (let j = 0; j < curves[code].length; j++) {
         const caseCount = curves[code][j][1];
         const percentage = (caseCount / population * 100).toFixed(3);
+        if (j == curves[code].length - 1) {
+          latestPercentageByCountry[code] = percentage;
+        }
         maxValue = Math.max(maxValue, percentage);
         thisData.push(percentage);
     }
     const color = Graphing.CURVE_COLORS[i % Graphing.CURVE_COLORS.length];
     dataToPlot.push({
       'data': thisData, 'label': name, 'borderColor': color,
+      'code': country.getCode(),
       'backgroundColor': 'transparent'});
     i++;
   }
+
+  let countriesSortedByAffectedness = Object.keys(latestPercentageByCountry);
+  countriesSortedByAffectedness.sort(function(a, b) {
+    return latestPercentageByCountry[b] > latestPercentageByCountry[a];
+  });
+
+  this.countriesSortedByAffectedness_ = countriesSortedByAffectedness;
+
   return [labels, dataToPlot];
 }
 
-filterGraphData(data) {
-  return data;
+filterGraphData(labelsAndData) {
+  const filterCount = parseInt(
+    document.getElementById('rank-filter-count').value, 10);
+  const filterSelect = document.getElementById('rank-filter-select').value;
+  console.log(filterCount);
+  console.log(filterSelect);
+  let filteredCurves = [];
+  if (filterSelect == 'populous') {
+    let selectedCountries = [];
+    for (let i = 0; i < filterCount; i++) {
+      selectedCountries.push(this.countriesSortedByPopulation_[i]);
+    }
+    for (let i = 0; i < labelsAndData[1].length; i++) {
+      if (selectedCountries.includes(labelsAndData[1][i]['code'])) {
+        filteredCurves.push(labelsAndData[1][i]);
+      }
+    }
+  } else {
+    // 'affected'
+    let selectedCountries = [];
+    for (let i = 0; i < filterCount; i++) {
+      selectedCountries.push(this.countriesSortedByAffectedness_[i]);
+    }
+    for (let i = 0; i < labelsAndData[1].length; i++) {
+      if (selectedCountries.includes(labelsAndData[1][i]['code'])) {
+        filteredCurves.push(labelsAndData[1][i]);
+      }
+    }
+  }
+  console.log(filteredCurves);
+  return [labelsAndData[0], filteredCurves];
 }
 
 renderFilters() {
   let container = document.getElementById('filters');
-  container.innerHTML = 'Only show the <input size="3" value="10" /> most ' +
-    '<select><option selected>affected</option>' +
+  container.innerHTML = 'Only show the ' +
+    '<input id="rank-filter-count" size="3" value="10" /> most ' +
+    '<select id="rank-filter-select"><option selected>affected</option>' +
     '<option>populous</option></select> ' +
     'countries';
+  document.getElementById('rank-filter-count').onchange = this.updateGraph.bind(this);
+  document.getElementById('rank-filter-select').onchange = this.updateGraph.bind(this);
+}
+
+cacheCountriesSortedByPopulation() {
+  let countries = this.dataProvider_.getCountries();
+  let codes = Object.keys(countries);
+  codes.sort(function(code_a, code_b) {
+    let a = countries[code_a];
+    let b = countries[code_b];
+    const pop_a = a.getPopulation();
+    const pop_b = b.getPopulation();
+    return pop_b - pop_a;
+  });
+  this.countriesSortedByPopulation_ = codes;
+}
+
+updateGraph() {
+  console.log('Updating graph');
+  const graphData = this.filterGraphData(this.prepareGraphData());
+  this.renderGraph(document.getElementById('app'), graphData[0], graphData[1]);
 }
 
 render() {
@@ -120,8 +199,7 @@ render() {
   container.innerHTML = '<h2>Confirmed cases in % of population. D = day of the ' +
       STARTING_CASE_COUNT.toLocaleString() + '<sup>th</sup> case</h2><div id="filters"></div>';
   this.renderFilters();
-  const graphData = this.filterGraphData(this.prepareGraphData());
-  this.renderGraph(container, graphData[0], graphData[1]);
+  this.updateGraph();
 }
 
 onConfigChanged(config) { };
